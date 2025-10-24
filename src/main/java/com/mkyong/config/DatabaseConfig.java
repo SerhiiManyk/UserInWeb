83a3c80322d9e4176bc3f02b0fc6f220a9c2d93c
Serhii Manyk <manukserg1988@gmail.com>
2025-10-24 17:25:02 +0200

    add spring-tx and spring-jdbs properties to pom

---
+++ src/main/java/com/mkyong/config/DatabaseConfig.java
@@ -0,0 +1,9 @@
+package com.mkyong.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class DatabaseConfig {
+}
