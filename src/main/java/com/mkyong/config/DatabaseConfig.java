4ad26a8e4c07b71811f443fb21d328428e46015b
Serhii Manyk <manukserg1988@gmail.com>
2025-10-24 17:29:24 +0200

    create DatabaseConfig

--- src/main/java/com/mkyong/config/DatabaseConfig.java
+++ src/main/java/com/mkyong/config/DatabaseConfig.java
@@ -1,9 +1,30 @@
 package com.mkyong.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
 public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/my_database");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("postgres");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
 }
+
