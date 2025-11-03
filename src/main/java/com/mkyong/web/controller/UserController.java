93c099e5809fb712471cdb55feb87343ecffeec5
Serhii Manyk <manukserg1988@gmail.com>
2025-11-03 10:16:44 +0100

    Create UserController

---
+++ src/main/java/com/mkyong/web/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.mkyong.web.controller;
+
+import com.mkyong.web.model.User;
+import org.springframework.ui.Model;
+import com.mkyong.web.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    public String showCreateForm(Model model){
+        model.addAttribute("user",new User());
+        return "create-user";
+    }
+
+    public String saveUser(@ModelAttribute("user") User user, Model model){
+        try{
+            userService.createUser(user);
+            model.addAttribute("message", "User successfully created!");
+            return "redirect:/";
+        } catch (Exception e) {
+            model.addAttribute("error", "Error creating user: " + e.getMessage());
+            return "create-user";
+        }
+    }
+}
