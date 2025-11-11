ccf7426c4c7fe4ae8414d8b8a982e504aaa72cba
Serhii Manyk <manukserg1988@gmail.com>
2025-11-11 17:59:16 +0100

    ToDoDAOImpl method searchToDoByCriteria() implementation

--- src/main/java/com/mkyong/web/dao/ToDoDAOImpl.java
+++ src/main/java/com/mkyong/web/dao/ToDoDAOImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -39,14 +40,32 @@ public class ToDoDAOImpl implements ToDoDAO{
 
 
     public Optional<ToDo> findToDoById(Long id) {
-        String sql = "SELECT * FROM todo WHERE id = ?";
+        String sql = "SELECT * FROM todos WHERE id = ?";
         List<ToDo> result = jdbcTemplate.query(sql, new ToDoRowMapper(), id);
         return result.stream().findFirst();
     }
 
 
     public List<ToDo> searchToDoByCriteria(ToDoSearchCriteria toDoCriteria) {
-        return List.of();
+        StringBuilder sql = new StringBuilder("SELECT id, userId, title, description, completed FROM todos WHERE 1=1");
+        List<Object> params = new ArrayList<>();
+
+        if (toDoCriteria.getUserId() != null) {
+            sql.append(" AND userId = ?");
+            params.add(toDoCriteria.getUserId());
+        }
+
+        if (toDoCriteria.getTitle() != null && !toDoCriteria.getTitle().isEmpty()) {
+            sql.append(" AND title ILIKE ?");
+            params.add("%" + toDoCriteria.getTitle() + "%");
+        }
+
+        if (toDoCriteria.getCompleted() != null ) {
+            sql.append(" AND completed = ?");
+            params.add(toDoCriteria.getCompleted());
+        }
+
+        return jdbcTemplate.query(sql.toString(), params.toArray(), new ToDoRowMapper());
     }
 
 
